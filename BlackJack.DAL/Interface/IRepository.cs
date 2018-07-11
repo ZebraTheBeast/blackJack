735b7d3446ed6a4e001c088fba1761d23f47ee4b
Lobyntsev Andrey <andreylobyntsev97@gmail.com>
2018-07-11 13:40:06 +0300

    Added BlackJack.DAL, BlackJack.MVC

---
+++ BlackJack.DAL/Interface/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.DAL.Interface
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Update(T entity);
+        void Remove(T entity);
+    }
+}
---
+++ BlackJack.DAL/Repository/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackJack.Entity;
+using System.Data.Entity;
+using BlackJack.DAL.Interface;
+
+namespace BlackJack.DAL.Repository
+{
+    public class Player : IRepository<Player>
+    {
+        private DbSet<Player> _players;
+
+        public void Add(Player entity)
+        {
+            _players.Add(entity);
+        }
+
+        public IEnumerable<Player> GetAll()
+        {
+            return _players;
+        }
+
+        public Player GetById(int id)
+        {
+            return _players.Find(id);
+        }
+
+        public void Remove(Player entity)
+        {
+            _players.Remove(entity);
+        }
+
+        public void Update(Player entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
