4f98bb18ab6ca440c293b22ed16fdb03ed8810c9
Lobyntsev Andrey <andreylobyntsev97@gmail.com>
2018-08-09 16:13:28 +0300

    Deleted useless files

--- BlackJack.API/Global.asax.cs
+++ BlackJack.API/Global.asax.cs
@@ -19,9 +19,6 @@ namespace BlackJack.API
       AreaRegistration.RegisterAllAreas();
       GlobalConfiguration.Configure(WebApiConfig.Register);
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-      RouteConfig.RegisterRoutes(RouteTable.Routes);
-      BundleConfig.RegisterBundles(BundleTable.Bundles);
-      
     }
   }
 }
