ab326dc7e2bfcba263289924df6d408198eed42c
Lobyntsev Andrey <andreylobyntsev97@gmail.com>
2018-08-20 17:29:55 +0300

    Refactored mvc

---
+++ BlackJack.MVC/Scripts/Site/Logs.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+
+	$("#grid").kendoGrid({
+		dataSource: {
+			transport: {
+				read: {
+					url: "/api/values/GetLogs",
+					type: "GET"
+				}
+			},
+			schema: {
+				type: "json",
+				model: {
+					fields: {
+						Id: { field: "Id", type: "number" },
+						Time: { field: "Time", type: "string" },
+						Message: { field: "Message", type: "string" }
+					}
+				}
+			},
+			pageSize: 14
+		},
+		pageable: true,
+		columns: [
+			{ field: "Id", width: "10%" },
+			{ field: "Message" },
+			{ field: "Time", width: "30%" }
+
+		]
+	});
+
+});
