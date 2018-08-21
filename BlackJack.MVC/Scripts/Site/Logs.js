ee6bec1b08aebe2257a91f743a2c8c52f8c179a3
Lobyntsev Andrey <andreylobyntsev97@gmail.com>
2018-08-21 10:01:02 +0300

    Added sorting and filtering in logs mvc

--- BlackJack.MVC/Scripts/Site/Logs.js
+++ BlackJack.MVC/Scripts/Site/Logs.js
@@ -12,21 +12,51 @@
 				type: "json",
 				model: {
 					fields: {
-						Id: { field: "Id", type: "number" },
-						Time: { field: "Time", type: "string" },
-						Message: { field: "Message", type: "string" }
+						Id: {
+							field: "Id",
+							type: "number"
+						},
+						Time: {
+							field: "Time",
+							type: "date"
+						},
+						Message: {
+							field: "Message",
+							type: "string"
+						}
 					}
 				}
 			},
 			pageSize: 14
 		},
 		pageable: true,
+		filterable: true,
 		columns: [
-			{ field: "Id", width: "10%" },
-			{ field: "Message" },
-			{ field: "Time", width: "30%" }
-
-		]
+			{
+				field: "Id",
+				width: "10%",
+				filterable: {
+					cell: {
+						showOperators: false
+					}
+				}
+			},
+			{
+				field: "Message",
+				filterable: {
+					cell: {
+						operator: "contains",
+						suggestionOperator: "contains"
+					}
+				}
+			},
+			{
+				field: "Time",
+				width: "30%",
+				format: "{0:dd.MM.yyyy HH:mm:ss}"
+			}
+		],
+		sortable: true
 	});
 
 });
