13408dad1fb815d5d8bd669894166fab3128d807
Lobyntsev Andrey <andreylobyntsev97@gmail.com>
2018-09-24 11:10:47 +0300

    Refactored card entity

--- BlackJack.Entity/Card.cs
+++ BlackJack.Entity/Card.cs
@@ -6,8 +6,6 @@ namespace BlackJack.Entities
 	[Table("Card")]
 	public class Card : BaseEntity
 	{
-		[ExplicitKey]
-		public override int Id { get; set; }
 		public string Title { get; set; }
 		public CardSuit Suit { get; set; }
 		public int Value { get; set; }
