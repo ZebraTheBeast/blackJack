624aba0a63a43db255ccc32ea60d0fe600b936e9
Lobyntsev Andrey <andreylobyntsev97@gmail.com>
2018-10-17 13:57:34 +0300

    Refactored StringHelper

--- BlackJack.Services/Helpers/StringHelper.cs
+++ BlackJack.Services/Helpers/StringHelper.cs
@@ -11,19 +11,37 @@
 
 		static public string OptionDraw = "You have draw! ";
 
-		public static string PlayerDrawCard(long cardId)
+        public static string PlayerNotInGame = "Player not in game";
+
+        public static string AlreadyBet = "You already placed a bet";
+
+        public static string PlayerDraw = "Player has draw with Dealer";
+
+        public static string EmptyName = "Name field is empty! Please, input your name in it";
+
+        public static string EmptyLog = "Logs are empty";
+
+        public static string BotJoinGame = "Bot join to the game";
+
+        public static string HumanJoinGame = "Human join to the game";
+
+        public static string BotsNotInGame = "There are no bots in the game";
+
+        public static string NoLastGame = "You don't have last game, start a new one!";
+
+        public static string DealerNotInGame = "There is no dealer in the game";
+
+        public static string NoBetValue = "Bet is not allowed.";
+
+        public static string MaxBotsAmount = "Too much bots";
+
+        public static string MinBotsAmount = "Too few bots";
+
+        public static string PlayerContinueGame = "Player continue game";
+
+        public static string PlayerDrawCard(long cardId)
 		{
 			return $"Player draw card with id {cardId}";
-		}
-
-		public static string PlayerNotInGame()
-		{
-			return "Player not in game";
-		}
-
-		public static string AlreadyBet()
-		{
-			return "You already placed a bet";
 		}
 
 		public static string NotEnoughPoints(int betValue)
@@ -41,21 +59,6 @@
 			return $"Player has value {playerValue} against {dealerValue}";
 		}
 
-		public static string PlayerDraw()
-		{
-			return $"Player has draw with Dealer";
-		}
-
-		public static string EmptyName()
-		{
-			return "Name field is empty! Please, input your name in it";
-		}
-
-		public static string EmptyLog()
-		{
-			return "Logs are empty";
-		}
-
 		public static string PlayerWin(int betValue)
 		{
 			return $"Player win {betValue} points";
@@ -66,57 +69,10 @@
 			return $"Player lose {betValue} points";
 		}
 
-		public static string BotJoinGame()
-		{
-			return "Bot join to the game";
-		}
-
-		public static string HumanJoinGame()
-		{
-			return $"Human join to the game";
-		}
-
-		public static string BotsNotInGame()
-		{
-			return "There are no bots in the game";
-		}
-
-		public static string NoLastGame()
-		{
-			return "You don't have last game, start a new one!";
-		}
-
-		public static string DealerNotInGame()
-		{
-			return "There is no dealer in the game";
-		}
-
-		public static string NoBetValue()
-		{
-			return "Bet is not allowed.";
-		}
-
-		public static string MaxBotsAmount()
-		{
-			return "Too much bots";
-		}
-
-		public static string MinBotsAmount()
-		{
-			return "Too few bots";
-		}
-
-		public static string PlayerContinueGame()
-		{
-			return "Player continue game";
-		}
-
 		static public string OptionSetBet(string message)
 		{
 			message += "Please, place a bet.";
 			return message;
 		}
-
-
 	}
 }
