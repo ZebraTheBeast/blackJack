98a2a6f0b22f5b7bee5316866d19c596a14920b8
Lobyntsev Andrey <andreylobyntsev97@gmail.com>
2018-10-17 13:10:43 +0300

    Added ignored file

---
+++ BlackJack.ViewModel/Log/GetLogsLogView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlackJack.ViewModels
+{
+	public class GetLogsLogView
+	{
+		public long Id { get; set; }
+		public DateTime CreationDate { get; set; }
+		public string Message { get; set; }
+		public long PlayerId { get; set; }
+		public long GameId { get; set; }
+	}
+}
