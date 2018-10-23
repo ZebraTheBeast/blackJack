5b73c0c180f3e6867dd4bf2a3b31f268abd10d95
Lobyntsev Andrey <andreylobyntsev97@gmail.com>
2018-10-23 13:44:48 +0300

    Deleted unused namespaces

--- BlackJack.WebApp/Startup.cs
+++ BlackJack.WebApp/Startup.cs
@@ -1,12 +1,8 @@
 ﻿using System.Web.Http;
-using Autofac.Integration.WebApi;
 using BlackJack.BusinessLogic.Mappers;
 using BlackJack.WebApp.Configs;
 using Microsoft.Owin;
 using Owin;
-using System.Web.Http.Cors;
-using Newtonsoft.Json.Serialization;
-using System.ComponentModel;
 using System.Web.Routing;
 
 [assembly: OwinStartup(typeof(BlackJack.WebApp.Startup))]
@@ -27,7 +23,6 @@ namespace BlackJack.WebApp
             app.UseAutofacMiddleware(container);
             app.UseAutofacWebApi(config);
             app.UseWebApi(config);
-
         }
     }
 }
